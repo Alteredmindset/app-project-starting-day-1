3a9d6e1bb56fd866619add273bbcc76e97343257
Altermindset <davoncudjoe88@hotmail.com>
2022-06-10 03:12:55 -0400

    day2

---
+++ src/components/ProfieCard/ProfileCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './ProfileCard.css'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.JPG'
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+        <div className="ProfileImages">
+            <img src={Cover} alt=''/>
+            <img src={Profile} alt=''/>
+        </div>
+
+        <div className="ProfileName">
+          <span>Luxi Chi</span>
+          <span>Senior Creative Director</span>
+        </div>
+
+
+        <div className="followStatus">
+          <hr />
+          <div>
+            <div className="follow">
+              <span>9,696</span>
+              <span>Following</span>
+            </div>
+            <div className='vL'></div>
+            <div className="follow">
+              <span>9</span>
+              <span>Followers</span>
+            </div>
+          </div>
+          <hr />
+        </div>
+        
+    </div>
+  )
+}
+
+export default ProfileCard
