2e7a59d6636199a98da27253331087c960eaebf7
alteredmindset <davoncujdoe88@homail.com>
2022-06-12 15:25:48 -0400

    day 3 part 2

---
+++ src/Data/PostsData.js
@@ -0,0 +1,28 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+export const PostsData = [
+
+    {
+        img: postPic1,
+        name: "Manny",
+        desc: "Congradulation on our lotto plus win! #2022",
+        likes:96969,
+        liked:true
+    },
+    {
+        img:postPic2,
+        name:"nick",
+        desc:"Wow dave really on the lotto plus?",
+        likes:96969,
+        liked:false
+    },
+    {
+        img:postPic3,
+        name:"slai",
+        desc:"Congradulation on our lotto plus win! #2022",
+        likes:96969,
+        liked:true
+    },
+]
